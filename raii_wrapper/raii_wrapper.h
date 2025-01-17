dbec94ddadd24522c11a3f05de9df5499b797eb1
orbabayof <orbabayof@gmail.com>
2025-01-17 18:31:15 +0200

    raii_wrapper is movable now ( meaning destructor func will not be called)

--- raii_wrapper/raii_wrapper.h
+++ raii_wrapper/raii_wrapper.h
@@ -11,19 +11,34 @@ public:
   using struct_t = T;
   using function_t = U;
 
-  raii_wrapper(const struct_t& Xstruct, const function_t& destractor): m_Xstruct { Xstruct }, m_destractor { destractor } 
+  raii_wrapper(const struct_t& Xstruct, const function_t& destractor): m_Xstruct { Xstruct }, m_destractor { destractor }, isMoved { false }
   {}
 
   ~raii_wrapper()
   {
-    m_destractor(m_Xstruct);  
+    if(!isMoved)
+      m_destractor(m_Xstruct);  
   }
 
   struct_t   m_Xstruct;
 
+    
+  raii_wrapper(raii_wrapper && another_wrapper) noexcept
+  {
+    m_Xstruct = another_wrapper.m_Xstruct;
+    another_wrapper.isMoved = true;
+  };
+  raii_wrapper(const raii_wrapper &) = delete;
+
+  operator struct_t()
+  {
+    return m_Xstruct;
+  }
+
 private:
 
-  function_t m_destractor   ;
+  function_t m_destractor ;
+  bool       isMoved      ; 
 };
 
 

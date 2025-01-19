5b874df4122dfc980d7f0fae7347c79c917f0881
orbabayof <orbabayof@gmail.com>
2025-01-19 07:38:14 +0200

    creating child proccess with boost::proccess::spawn instead of execvp and fork, all bugs fixed

--- settings/settings.cpp
+++ settings/settings.cpp
@@ -1,36 +1,30 @@
+
+#include <boost/process/v1/detail/child_decl.hpp>
+#include <boost/process/v1/search_path.hpp>
+#include <keybind.h>
+#include <settings.h>
+
 #include <X11/X.h>
 #include <X11/Xlib.h>
 #include <X11/keysymdef.h>
 #include <cstdlib>
 #include <functional>
 #include <iostream>
-#include <keybind.h>
-#include <settings.h>
 #include <string_view>
 #include <unistd.h>
 
-template <std::size_t N>
-int execvp(const char* file, const char* const (&argv)[N])
-{
-  return execvp(file, const_cast<char* const*>(argv));
-}
+#include <boost/process.hpp>
+
 
 
 inline std::function<void()> spawn(std::string_view program_name)
 {
 	return {[=]() {
+
+    namespace bp = boost::process;
 		std::cout << "running spawn cmd\n";
-
-		const char* argument_list[] = {nullptr};
-
-  	pid_t pid { fork() };
-
-  	if(pid)
-    {
-	  	execvp(program_name.data(), argument_list);
-      std::exit(0);
-    }
-
+    
+    bp::spawn(bp::search_path(program_name));
 	}};
 }
 
@@ -43,6 +37,6 @@ void settings::initKeyBinds()
 	using enum window_t;
 
 	static keybind<wm> first{"w", Mod1Mask, spawn("google-chrome-stable")};
-  /*static keybind<wm> second{"a", AnyModifier, spawn("arduino")};*/
+  static keybind<wm> second{"a", AnyModifier, spawn("arduino")};
   /*std::cout << first.keycode() << " : " << first.modifiers() << '\n';*/
 }
